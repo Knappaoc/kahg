12b990a6405e505b277a7950091907a1a1185a5a
Kah Goh <villastar@yahoo.com.au>
2022-11-24 21:44:33 +0800

    Alternate more compact solution for 2021 exercise 1

--- 2021/solution1.exs
+++ 2021/solution1.exs
@@ -1,16 +1,11 @@
 defmodule Functions do
-
-  def window(x, []), do: [x]
-  def window(x, [a]), do: [a, x]
-  def window(x, [_, b]), do: [b, x]
-
-  def filter([a, b], acc) when a < b, do: acc+1
-  def filter(_, acc), do: acc
+  def window(x, {count, a}) when a < x, do: {count + 1, x}
+  def window(x, {count, _}), do: {count, x}
 end
 
-File.stream!("inputs", [:utf8], :line)
+{c, _} = File.stream!("inputs1", [:utf8], :line)
   |> Stream.map(&String.trim/1)
   |> Stream.map(&String.to_integer/1)
-  |> Stream.scan([], &Functions.window/2)
-  |> Enum.reduce(0, &Functions.filter/2)
-  |> IO.puts()
+  |> Enum.reduce({0, nil}, &Functions.window/2)
+
+IO.puts(c)
