d9ba5e6409e25605ad3a6fd948d423d367f2b004
Kah Goh <villastar@yahoo.com.au>
2022-12-07 07:25:22 +0800

    Use Elixir range in day 4

--- day04/solution.exs
+++ day04/solution.exs
@@ -3,32 +3,21 @@ defmodule Functions do
 
   defp parse_range(range) do
     [r_start, r_end] = String.split(range, "-")
-    {String.to_integer(r_start), String.to_integer(r_end)}
+    String.to_integer(r_start)..String.to_integer(r_end)
   end
 
   @doc """
   Returns true if one range is wholly inside the other
   """
-  def overlap?({{start1, end1}, {start2, end2}}) when start1 <= start2 and end2 <= end1, do: true
-  def overlap?({{start1, end1}, {start2, end2}}) when start2 <= start1 and end1 <= end2, do: true
-  def overlap?({_elf1, _elf2}), do: false
-
-  @doc """
-  Returns true if the sets DON'T overlap
-  """
-  # Elf 1 range < Elf 2 range
-  def disjoint?({{start1, end1}, {start2, _end2}}) when start1 < start2 and end1 < start2, do: true
-  # Elf 2 range < Elf 1 range
-  def disjoint?({{start1, end1}, {_start2, end2}}) when start1 > end2 and end1 > end2, do: true
-  # Anything else, there is overlap
-  def disjoint?({_elf1, _elf2}), do: false
+  def overlap?({elf1, elf2}) do
+    Enum.all?(elf1, fn x -> x in elf2 end) || Enum.all?(elf2, fn x -> x in elf1 end)
+  end
 end
 
 pairs = File.stream!("input", [:utf8], :line)
 |> Stream.map(&String.trim/1)
 |> Stream.map(&(String.split(&1, ",")))
-|> Stream.map(&Functions.parse_pairs/1)
-|> Enum.into([])
+|> Enum.map(&Functions.parse_pairs/1)
 
 # Part 1 - Count of pairs where one pair is wholly inside the other
 pairs
@@ -37,6 +26,6 @@ pairs
 
 # Part 2 - Count of pairs with ANY overlap (or things that DON'T have NO OVERLAP)
 pairs
-|> Enum.reject(&Functions.disjoint?/1)
+|> Enum.reject(fn {elf1, elf2} -> Range.disjoint?(elf1, elf2) end)
 |> Enum.count()
 |> IO.inspect(label: "Count of any overlap")
